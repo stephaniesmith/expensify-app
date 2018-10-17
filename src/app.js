a5eca2f8a563a3eb2211f9673967c83e8ced7ae1
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-10-17 10:15:55 -0700

    setting up a nav

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -28,13 +28,33 @@ const Help = () => (
   </div>
 )
 
+const NotFound = () => (
+  <div>
+    404 - <Link to="/">Go Home</Link>
+  </div>
+)
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
+    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+  </header>
+)
+
 const routes = (
   <BrowserRouter>
     <div>
-      <Route path="/" component={ExpenseDashboard} exact={true}/>
-      <Route path="/create" component={AddExpense}/>
-      <Route path="/edit" component={Edit}/>
-      <Route path="/help" component={Help}/>
+      <Header/>
+      <Switch>
+        <Route path="/" component={ExpenseDashboard} exact={true}/>
+        <Route path="/create" component={AddExpense}/>
+        <Route path="/edit" component={Edit}/>
+        <Route path="/help" component={Help}/>
+        <Route component={NotFound}/>
+        </Switch>
     </div>
   </BrowserRouter>
 );
