2e81cdff652e37fc876d9293064e2f0c05ccaac3
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-10-17 09:26:42 -0700

    setting up a router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Hello World!</p>, document.getElementById('app'));
+const ExpenseDashboard = () => (
+  <div>
+    DASHBOARD!
+  </div>
+)
+
+const AddExpense = () => (
+  <div>
+    EXPENSE!
+  </div>
+)
+
+const Edit = () => (
+  <div>
+    EDIT!
+  </div>
+)
+
+const Help = () => (
+  <div>
+    HELP!
+  </div>
+)
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboard} exact={true}/>
+      <Route path="/create" component={AddExpense}/>
+      <Route path="/edit" component={Edit}/>
+      <Route path="/help" component={Help}/>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
