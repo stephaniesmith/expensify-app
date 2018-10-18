0a62731ee4c2bf18a535b5794344b698e9fee5e3
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-10-18 09:59:55 -0700

    dispatching actions

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,44 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+
+  switch(action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      }
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      }
+    case 'RESET':
+      return {
+        count: 0
+      }
+    default:
+      return state;
+  }
 });
+
+console.log(store.getState());
+
+store.dispatch({
+  type: 'INCREMENT',
+  // payload: 1
+})
+
+console.log(store.getState());
+
+store.dispatch({
+  type: 'RESET',
+  // payload: 1
+})
+
+console.log(store.getState());
+
+store.dispatch({
+  type: 'DECREMENT',
+  // payload: 1
+})
 
 console.log(store.getState());
