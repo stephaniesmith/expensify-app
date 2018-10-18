ca6b341e1e0b46671e0466b352f2a68e6b5a4588
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-10-18 07:34:24 -0700

    refactoring the router

--- src/app.js
+++ src/app.js
@@ -1,62 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboard = () => (
-  <div>
-    DASHBOARD!
-  </div>
-)
-
-const AddExpense = () => (
-  <div>
-    EXPENSE!
-  </div>
-)
-
-const Edit = () => (
-  <div>
-    EDIT!
-  </div>
-)
-
-const Help = () => (
-  <div>
-    HELP!
-  </div>
-)
-
-const NotFound = () => (
-  <div>
-    404 - <Link to="/">Go Home</Link>
-  </div>
-)
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-    <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-  </header>
-)
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Header/>
-      <Switch>
-        <Route path="/" component={ExpenseDashboard} exact={true}/>
-        <Route path="/create" component={AddExpense}/>
-        <Route path="/edit" component={Edit}/>
-        <Route path="/help" component={Help}/>
-        <Route component={NotFound}/>
-        </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter/>, document.getElementById('app'));
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from '../components/Header';
+import ExpenseDashboard from '../components/ExpenseDashboard';
+import AddExpense from '../components/AddExpense';
+import EditExpense from '../components/EditExpense';
+import Help from '../components/Help';
+import NotFound from '../components/NotFound';
+
+const ReactRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header/>
+      <Switch>
+        <Route path="/" component={ExpenseDashboard} exact={true}/>
+        <Route path="/create" component={AddExpense}/>
+        <Route path="/help" component={Help}/>
+        <Route component={NotFound}/>
+        </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default ReactRouter;
