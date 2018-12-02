224d937a04966a37c97fb02f166cc38db2ee67d7
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-02 13:52:58 -0800

    refactoring the addExpense component

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';;
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { addExpense } from '../actions/expenses';
 
-const AddExpense = props => (
-  <div>
-    <h1>Add Expense</h1>
-    <ExpenseForm
-      onSubmit={expense => {
-        props.dispatch(addExpense(expense));
-        props.history.push('/');
-      }}
-    />
-  </div>
-);
+export class AddExpense extends Component {
 
-export default connect()(AddExpense);
+  onSubmit = expense => {
+    this.props.onSubmit(expense);
+    this.props.history.push('/');
+  };
+  render() {
+    return (
+      <div>
+        <h1>Add Expense</h1>
+        <ExpenseForm
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  onSubmit: expense => dispatch(addExpense(expense))
+});
+
+export default connect(undefined, mapDispatchToProps)(AddExpense);
