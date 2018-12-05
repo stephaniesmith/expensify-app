4458f00061aad9728c79c4b842ca55bea789ee98
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-05 15:55:21 -0800

    added another test

--- src/tests/selectors/expenses-totals.test.js
+++ src/tests/selectors/expenses-totals.test.js
@@ -6,6 +6,11 @@ test('should return 0 if no expenses', () => {
   expect(res).toEqual(0);
 });
 
+test('should return total of one expense', () => {
+  const res = selectExpensesTotal([expenses[0]]);
+  expect(res).toEqual(195);
+});
+
 test('should return total of all expenses', () => {
   const res = selectExpensesTotal(expenses);
   expect(res).toEqual(114195);
