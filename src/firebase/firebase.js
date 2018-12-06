0ac9e3d18e42f24bfc490e9371d5c8ccbe876468
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-06 12:53:50 -0800

    more firebase stuff

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,31 +13,62 @@ firebase.initializeApp(config);
 
 const { database } = firebase;
 
-database().ref().set({
-  name: 'Stephanie Smith',
-  stressLevel: 5,
-  age: 28,
-  job: {
-    title: 'Dev',
-    company: 'Google'
-  },
-  location: {
-    city: 'Salem',
-    country: 'United States'
-  }
-})
-  .then(() => {
-    console.log('Data is saved!!! ');
-  })
-  .catch(err => {
+const onValueChange = database()
+  .ref()
+  .on('value', snapshot => {
+    console.log(snapshot.val());
+  }, err => {
     console.error(err);
   });
 
-database().ref().update({
-  stressLevel: 9,
-  'job/company': 'Amazon',
-  'location/city': 'Portland'
-});
+setTimeout(() => {
+  database().ref('age').set(29);
+}, 3500);
+
+setTimeout(() => {
+  database().ref().off(onValueChange);
+}, 7000);
+
+setTimeout(() => {
+  database().ref('age').set(31);
+}, 10500);
+  
+// database()
+//   .ref('location/city')
+//   .once('value')
+//   .then(snapshot => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+
+// database().ref().set({
+//   name: 'Stephanie Smith',
+//   stressLevel: 5,
+//   age: 28,
+//   job: {
+//     title: 'Dev',
+//     company: 'Google'
+//   },
+//   location: {
+//     city: 'Salem',
+//     country: 'United States'
+//   }
+// })
+//   .then(() => {
+//     console.log('Data is saved!!! ');
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+
+// database().ref().update({
+//   stressLevel: 9,
+//   'job/company': 'Amazon',
+//   'location/city': 'Portland'
+// });
 
 // database()
 //   .ref()
