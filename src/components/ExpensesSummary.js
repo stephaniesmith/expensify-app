06cf1577dfeb105e1ea3c01cf4ac8edf91755936
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-12-05 19:24:57 -0800

    setting up a new component

---
+++ src/components/ExpensesSummary.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import numeral from 'numeral';
+
+const ExpensesSummary = ({ expenseCount, expensesTotal}) => {
+  const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+  const formatedTotal = numeral(expensesTotal / 100).format('$0,0.00');
+  return (
+    <div>
+      <h1>Viewing {expenseCount} {expenseWord} totalling {formatedTotal}</h1>
+    </div>
+  );
+};
+
+export default ExpensesSummary;
+
