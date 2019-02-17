8942711c06a6574841f11c24f09aaa55e4b2dc8b
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-17 13:40:13 -0800

    getting test and dev env setup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyCE-3GEsZkdPkAdDl4PHUHv5Lr0jSqGEhM',
-  authDomain: 'alchemy-expensify.firebaseapp.com',
-  databaseURL: 'https://alchemy-expensify.firebaseio.com',
-  projectId: 'alchemy-expensify',
-  storageBucket: 'alchemy-expensify.appspot.com',
-  messagingSenderId: '614558370242'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
