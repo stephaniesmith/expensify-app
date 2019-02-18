601a7b63e38f46069e166a6f30220d78a2bfd0d5
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-18 13:25:57 -0800

    Styling the login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,8 +5,12 @@ import { startLogin } from '../actions/auth';
 
 export const Login = ({ startLogin }) => {
   return (
-    <div>
-      <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+      <div className="box-layout__box">
+        <h1 className="box-layout__title">Expensify App</h1>
+        <p>It's time to get your expenses under control.</p>
+        <button onClick={startLogin}>Login</button>
+      </div>
     </div>
   );
 };
