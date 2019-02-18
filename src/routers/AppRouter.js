02385b9b9fff79693b49b4c52535cd4ab2460011
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-18 10:47:09 -0800

    Getting the private route setup

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-import Header from '../components/Header';
 import ExpenseDashboard from '../components/ExpenseDashboard';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
 import Help from '../components/Help';
 import NotFound from '../components/NotFound';
 import Login from '../components/Login';
+import PrivateRoute from './PrivateRoute';
 
 export const history = createHistory();
 
 const ReactRouter = () => (
   <Router history={history}>
     <div>
-      <Header/>
       <Switch>
         <Route path="/" component={Login} exact={true}/>
-        <Route path="/dashboard" component={ExpenseDashboard}/>
-        <Route path="/create" component={AddExpense}/>
+        <PrivateRoute path="/dashboard" component={ExpenseDashboard}/>
+        <PrivateRoute path="/create" component={AddExpense}/>
         <Route path="/help" component={Help}/>
-        <Route path="/edit/:id" component={EditExpense}/>
+        <PrivateRoute path="/edit/:id" component={EditExpense}/>
         <Route component={NotFound}/>
       </Switch>
     </div>
