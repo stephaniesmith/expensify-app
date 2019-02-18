bc8b7f4c89b3ccfbfd8e41bbfca61ffbf2607c9f
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-18 15:45:15 -0800

    page header styles

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
@@ -9,8 +10,13 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
   const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
   const formatedTotal = numeral(expensesTotal / 100).format('$0,0.00');
   return (
-    <div>
-      <h1>Viewing {expenseCount} {expenseWord} totalling {formatedTotal}</h1>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formatedTotal}</span></h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
   );
 };
